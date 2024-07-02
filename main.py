128f8ce4447586ab428e8acaa166d4c5d8cd224c
SergBo77 <seman77@mail.ru>
2024-07-02 21:28:43 +0300

    add work

--- main.py
+++ main.py
@@ -1,37 +1,50 @@
+# Импортируем библиотеки
 import time
 import csv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+# Инициализация драйвера Chrome
 driver = webdriver.Chrome()
 
+# URL-адрес для парсинга
 url = "https://divan.ru/category/svet"
 
+# Переход по URL-адресу в браузере
 driver.get(url)
-time.sleep(3)
+time.sleep(3)  # Пауза для загрузки страницы
 
+# Находим все элементы, соответствующие классу 'lsooF'
 lights = driver.find_elements(By.CLASS_NAME, 'lsooF')
 
 parsed_data = []
 
+# Проходимся по каждому элементу
 for light in lights:
     try:
+        # Получаем наименование товара
         name = light.find_element(By.CSS_SELECTOR, 'span[itemprop="name"]').text
+        # Получаем цену товара
         price = light.find_element(By.CSS_SELECTOR, 'span.ui-LD-ZU').text
+        # Получаем ссылку на товар
         link = light.find_element(By.CSS_SELECTOR, 'link[itemprop="url"]').get_attribute('href')
     except:
-          print("Произошла ошибка при парсинге")
-          continue
+        print("Произошла ошибка при парсинге")
+        continue
 
-    parsed_data.append([name,  price, link])
+    # Добавляем данные в список parsed_data
+    parsed_data.append([name, price, link])
 
+# Закрываем браузер
 driver.quit()
 
-with open("lights_divan.csv", 'w',newline='', encoding='utf-8') as file:
-     writer = csv.writer(file)
-     writer.writerow(['Наименование', 'Цена', 'Ссылка на товар'])
-     writer.writerows(parsed_data)
-
+# Записываем данные в CSV файл
+with open("lights_divan.csv", 'w', newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
+    # Записываем заголовки столбцов
+    writer.writerow(['Наименование', 'Цена', 'Ссылка на товар'])
+    # Записываем данные
+    writer.writerows(parsed_data)
 
 
 
